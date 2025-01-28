dd752cb83cd135d1e35941f87ad1f7458f32f34c
Ömer Faruk <farukcelik2534@gmail.com>
2025-01-29 01:36:30 +0300

    common projects added

--- src/Catalog/Udemy.Catalog.Domain/GlobalUsings.cs
+++
@@ -1,1 +0,0 @@
-﻿global using Udemy.Common.Models.Entities;
--- src/Catalog/Udemy.Catalog.Domain/Models/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-using Udemy.Common.Models.Entities.Base;
-
-namespace Udemy.Catalog.Domain.Models.Entities;
-
-public class Category : BaseEntity<int>
-{
-    public string Name { get; set; } = string.Empty;
-}
