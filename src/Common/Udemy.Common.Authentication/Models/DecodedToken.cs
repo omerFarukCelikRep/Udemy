60dbdc9c67348061dc67fee84886d53b5e5488a5
Ömer Faruk <farukcelik2534@gmail.com>
2025-01-29 01:37:41 +0300

    Common.Authentication - Decoded Token class added

---
+++ src/Common/Udemy.Common.Authentication/Models/DecodedToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Udemy.Common.Authentication.Models;
+public record DecodedToken(string KeyId,
+                           string Issuer,
+                           IReadOnlyCollection<string> Audience,
+                           Dictionary<string, string> Claims,
+                           DateTime ValidTo,
+                           string SignatureAlgorithm,
+                           string RawData,
+                           string Subject,
+                           DateTime ValidFrom,
+                           string EncodedHeader,
+                           string EncodedPayload);
