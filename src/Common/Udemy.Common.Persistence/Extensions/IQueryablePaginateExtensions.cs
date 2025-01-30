90b14aa3067b2d6013ac5dd150df35132d973fed
Ömer Faruk <farukcelik2534@gmail.com>
2025-01-30 17:14:15 +0300

    Common.Models - IQueryable paginate extension added

---
+++ src/Common/Udemy.Common.Persistence/Extensions/IQueryablePaginateExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Udemy.Common.Models.Models.Pagination;
+
+namespace Udemy.Common.Persistence.Extensions;
+public static class IQueryablePaginateExtensions
+{
+    public static async Task<Paginate<T>> ToPaginateAsync<T>(this IQueryable<T> source, int index, int size, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        EnsureInRange(index, size);
+
+        int count = await source.CountAsync(cancellationToken)
+                                .ConfigureAwait(false);
+
+        IQueryable<T> items = source.Take((index * size)..size);
+
+        return new Paginate<T>(items, index, size, count);
+    }
+
+    private static void EnsureInRange(int index, int size)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index);
+        ArgumentOutOfRangeException.ThrowIfLessThan(index, default);
+
+        ArgumentOutOfRangeException.ThrowIfNegative(size);
+        ArgumentOutOfRangeException.ThrowIfLessThan(size, default);
+    }
+}
