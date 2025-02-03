eef34d40417f04e6993c9f7080d8a3db0d0fb7f3
Ömer Faruk <farukcelik2534@gmail.com>
2025-02-04 00:59:43 +0300

    Common.Logging - Serilog configurations added

---
+++ src/Common/Udemy.Common.Logging/Extensions/ILoggerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace Udemy.Common.Logging.Extensions;
+public static class ILoggerExtension
+{
+    private const string Path = "../Logs/Logs.txt";
+
+    public static void UseSerilog(this IHostBuilder hostBuilder, params string[] headers) => hostBuilder.UseSerilog((context, loggerConfiguration) =>
+                                                                         {
+                                                                             loggerConfiguration.MinimumLevel.Debug();
+                                                                             loggerConfiguration.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+                                                                             loggerConfiguration.MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Error);
+                                                                             loggerConfiguration.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information);
+
+                                                                             loggerConfiguration.WriteTo.Console(formatter: new CompactJsonFormatter(), restrictedToMinimumLevel: LogEventLevel.Debug);
+
+                                                                             loggerConfiguration.WriteTo.File(path: Path,
+                                                                                                              formatter: new CompactJsonFormatter(),
+                                                                                                              restrictedToMinimumLevel: LogEventLevel.Debug,
+                                                                                                              rollingInterval: RollingInterval.Day,
+                                                                                                              rollOnFileSizeLimit: true);
+
+                                                                             loggerConfiguration.Enrich.WithMachineName();
+                                                                             loggerConfiguration.Enrich.WithProcessId();
+                                                                             loggerConfiguration.Enrich.WithThreadId();
+                                                                             loggerConfiguration.Enrich.WithClientIp();
+                                                                             loggerConfiguration.Enrich.WithCorrelationId();
+                                                                             foreach (string header in headers)
+                                                                             {
+                                                                                 loggerConfiguration.Enrich.WithRequestHeader(header);
+                                                                             }
+                                                                         });
+}
