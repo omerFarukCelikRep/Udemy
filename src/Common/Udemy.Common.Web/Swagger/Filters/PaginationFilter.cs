e3b93998f8118fac60d81c67b35864afb8a00a18
Ömer Faruk <farukcelik2534@gmail.com>
2025-02-03 17:46:58 +0300

    Common.Web - class library created and swagger header filter added

---
+++ src/Common/Udemy.Common.Web/Swagger/Filters/PaginationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Udemy.Common.Models.Constants;
+
+namespace Udemy.Common.Web.Swagger.Filters;
+public class PaginationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context) => operation.Parameters =
+        [
+            new OpenApiParameter
+            {
+                Name = Header.XPage,
+                In = ParameterLocation.Header,
+                Description = "Pagination Page Index Header",
+                Required = false
+            },
+            new OpenApiParameter
+            {
+                Name = Header.XPageSize,
+                In = ParameterLocation.Header,
+                Description = "Pagination Page Size Header",
+                Required = false
+            }
+        ];
+}
