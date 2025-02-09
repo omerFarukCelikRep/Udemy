f6a5bd89d5322eaf57adc8c8e0b8e33cacbd67a6
Ömer Faruk <farukcelik2534@gmail.com>
2025-02-09 15:30:06 +0300

    Common.Logging - Request Logging pipeline behavior added

---
+++ src/Common/Udemy.Common.Logging/Behaviors/RequestResponseLoggingBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Udemy.Common.Logging.Behaviors;
+public class RequestResponseLoggingBehavior<TRequest, TResponse>(ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var correlationId = Guid.CreateVersion7();
+        string requestJson = JsonSerializer.Serialize(request);
+
+        logger.LogInformation("Handling request {CorrelationID}: {Request}", correlationId, requestJson);
+
+        TResponse? response = await next();
+        string responseJson = JsonSerializer.Serialize(response);
+
+        logger.LogInformation("Response for {Correlation}: {Response}", correlationId, responseJson);
+
+        return response;
+    }
+}
