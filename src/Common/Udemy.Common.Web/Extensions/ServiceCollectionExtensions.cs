01d811505b4eef0121c5f02654cd3902f3e0e08c
Ömer Faruk <farukcelik2534@gmail.com>
2025-02-10 22:16:15 +0300

    Common.Web - Cors options added

--- src/Common/Udemy.Common.Web/Extensions/ServiceCollectionExtensions.cs
+++ src/Common/Udemy.Common.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,49 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Udemy.Common.Web.Handlers;
+using Udemy.Common.Web.Options;
 
 namespace Udemy.Common.Web.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddGlobalExceptionHandler(this IServiceCollection services)
+    public static IServiceCollection AddGlobalExceptionHandler(this IServiceCollection services)
     {
         services.AddExceptionHandler<GlobalExceptionHandler>();
         services.AddProblemDetails();
+
+        return services;
+    }
+
+    public static IServiceCollection AddCors(this IServiceCollection services, IReadOnlyList<string>? policyNames = default(List<string>))
+    {
+        services.AddOptions<CorsOptions>()
+               .BindConfiguration(CorsOptions.SectionName);
+
+        ServiceProvider serviceProvider = services.BuildServiceProvider();
+        CorsOptions corsOptions = serviceProvider.GetRequiredService<IOptions<CorsOptions>>().Value;
+
+        services.AddCors(options =>
+        {
+            if (policyNames is not { Count: > 0 })
+                options.AddPolicy(CorsPolicyOptions.DefaultSectionName, builder => ConfigurePolicy(builder, CorsPolicyOptions.DefaultSectionName));
+
+            foreach (string policyName in policyNames!)
+            {
+                options.AddPolicy(policyName, builder => ConfigurePolicy(builder, policyName));
+            }
+
+            options.DefaultPolicyName = CorsPolicyOptions.DefaultSectionName;
+        });
+
+        return services;
+
+        void ConfigurePolicy(Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicyBuilder builder, string policyName)
+        {
+            CorsPolicyOptions policyOptions = corsOptions.Policies[policyName];
+            builder.WithOrigins(policyOptions.Origins);
+            builder.WithHeaders(policyOptions.Headers);
+            builder.WithMethods(policyOptions.Methods);
+        }
     }
 }
---
+++ src/Common/Udemy.Common.Web/Options/CorsOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace Udemy.Common.Web.Options;
+public class CorsOptions
+{
+    public const string SectionName = "Cors";
+
+    public Dictionary<string, CorsPolicyOptions> Policies { get; } = null!;
+}
+
+public class CorsPolicyOptions
+{
+    public const string DefaultSectionName = "Default";
+
+    public string[] Origins { get; set; } = null!;
+
+    public string[] Headers { get; set; } = null!;
+
+    public string[] Methods { get; set; } = null!;
+}
